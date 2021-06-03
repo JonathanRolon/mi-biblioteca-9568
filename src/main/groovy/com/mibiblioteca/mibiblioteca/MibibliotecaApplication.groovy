ac7048e667758e7a2f536bafcf3311edc9cb2ed7
JonathanRolon <rolon.jonathan.ezequiel@gmail.com>
2021-06-03 00:47:07 -0300

    first commit

---
+++ src/main/groovy/com/mibiblioteca/mibiblioteca/MibibliotecaApplication.groovy
@@ -0,0 +1,13 @@
+package com.mibiblioteca.mibiblioteca
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MibibliotecaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MibibliotecaApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/mibiblioteca/mibiblioteca/MibibliotecaApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.mibiblioteca.mibiblioteca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MibibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
